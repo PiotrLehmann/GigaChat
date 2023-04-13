bffa1955a37035b76631a990047ee6f23e4a4a3f
PiotrLehmann <piterrubik@gmail.com>
2023-04-14 00:59:15 +0200

    login and signup content created

---
+++ frontend/src/components/Authentication/Login.js
@@ -0,0 +1,58 @@
+import React from "react";
+import {
+  Button,
+  FormControl,
+  FormLabel,
+  Input,
+  InputGroup,
+  InputRightElement,
+  VStack,
+} from "@chakra-ui/react";
+import { useState } from "react";
+
+const Login = () => {
+  const [name, setName] = useState();
+  const [email, setEmail] = useState();
+  const [password, setPassword] = useState();
+  const [show, setShow] = useState(false);
+
+  const submitHandler = () => {};
+
+  const handleClick = () => {
+    setShow(!show);
+  };
+
+  return (
+    <div>
+      <VStack spacing="5px">
+        <FormControl id="email" isRequired>
+          <FormLabel>Email</FormLabel>
+          <Input
+            placeholder="Enter your email"
+            onChange={(e) => setEmail(e.target.value)}
+          />
+        </FormControl>
+        <FormControl id="password" isRequired>
+          <FormLabel>Password</FormLabel>
+          <InputGroup>
+            <Input
+              type={show ? "text" : "password"}
+              placeholder="Enter your password"
+              onChange={(e) => setPassword(e.target.value)}
+            />
+            <InputRightElement>
+              <Button h="1.75rem" size="sm" onClick={handleClick}>
+                {show ? "Hide" : "Show"}
+              </Button>
+            </InputRightElement>
+          </InputGroup>
+        </FormControl>
+        <Button width="100%" onClick={submitHandler}>
+          Sign Up
+        </Button>
+      </VStack>
+    </div>
+  );
+};
+
+export default Login;
--- frontend/src/pages/Homepage.js
+++ frontend/src/pages/Homepage.js
@@ -1,9 +1,51 @@
-import React from 'react'
+import React from "react";
+import {
+  Container,
+  Box,
+  Center,
+  Text,
+  Tab,
+  Tabs,
+  TabList,
+  TabPanels,
+  TabPanel,
+} from "@chakra-ui/react";
+import Login from "../components/Authentication/Login";
+import SignUp from "../components/Authentication/SignUp";
 
 const Homepage = () => {
   return (
-    <div>Homepage</div>
-  )
-}
+    <Container maxW="xl">
+      <Center
+        d="flex"
+        justifyContent="center"
+        p={3}
+        bg={"grey"}
+        w="100%"
+        m="40px 0 15px 0"
+        borderRadius="1g"
+        borderWidth="1px"
+      >
+        <Text>GigaChat</Text>
+      </Center>
+      <Box bg="grey" w="100%" p={4} borderRadius="1g" borderWidth="1px">
+        <Tabs variant="soft-rounded">
+          <TabList mb="1em">
+            <Tab width="50%">Login</Tab>
+            <Tab width="50%">Sign up</Tab>
+          </TabList>
+          <TabPanels>
+            <TabPanel>
+              <Login />
+            </TabPanel>
+            <TabPanel>
+              <SignUp />
+            </TabPanel>
+          </TabPanels>
+        </Tabs>
+      </Box>
+    </Container>
+  );
+};
 
-export default Homepage
+export default Homepage;
