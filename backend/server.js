44b8a0340ebebde1c4b481d73d23376cfcbc3784
PiotrLehmann <piterrubik@gmail.com>
2023-04-14 01:25:29 +0200

    mongodb connected

--- backend/server.js
+++ backend/server.js
@@ -1,23 +1,24 @@
-const express = require('express');
-const dotenv = require('dotenv');
-const { chats } = require('./data/data');
+const express = require("express");
+const dotenv = require("dotenv");
+const { chats } = require("./data/data");
+const connectDB = require("./config/db");
 
 dotenv.config();
-
+connectDB();
 const app = express();
 
-app.get('/', (req, res) => {
-    res.send("API runnin!!!");
+app.get("/", (req, res) => {
+  res.send("API runnin!!!");
 });
 
-app.get('/api/chat', (req, res) => {
-    res.send(chats);
+app.get("/api/chat", (req, res) => {
+  res.send(chats);
 });
 
-app.get('/api/chat/:id', (req, res) => {
-    // console.log(req.params.id);
-    const singleChat = chats.find((c) => c._id === req.params.id)
-    res.send(singleChat);
+app.get("/api/chat/:id", (req, res) => {
+  // console.log(req.params.id);
+  const singleChat = chats.find((c) => c._id === req.params.id);
+  res.send(singleChat);
 });
 
 const PORT = process.env.PORT || 5000;
