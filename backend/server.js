65bb26e979ba51bd2e4d288938586e6671ee1793
PiotrLehmann <piterrubik@gmail.com>
2023-04-14 03:10:00 +0200

    Password encryption with bcrypt done

--- backend/server.js
+++ backend/server.js
@@ -19,4 +19,4 @@ app.use("/api/user", userRoutes);
 
 const PORT = process.env.PORT || 5000;
 
-app.listen(PORT, console.log(`Server kurwa on port ${PORT}`));
+app.listen(PORT, console.log(`Serverrr on port ${PORT}`));
