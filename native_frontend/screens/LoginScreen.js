2eda7fba15cdf68c8ac193774a0720945212b102
PiotrLehmann <piterrubik@gmail.com>
2023-04-14 15:52:48 +0200

    LoginScreen logic not working

--- native_frontend/screens/LoginScreen.js
+++ native_frontend/screens/LoginScreen.js
@@ -7,35 +7,86 @@ import {
   Input,
   NativeBaseProvider,
   Container,
+  useToast,
 } from "native-base";
 import React, { useState } from "react";
-export const LoginScreen = () => {
-  const [show, setShow] = useState(false);
+import axios from "axios";
 
-  const handleClick = () => setShow(!show);
+export const LoginScreen = () => {
+  const [email, setEmail] = useState();
+  const [password, setPassword] = useState();
+  const [show, setShow] = useState(false);
+  const toast = useToast();
+  const [loading, setLoading] = useState();
+
+  const handleClick = () => {
+    setShow(!show);
+  };
+
+  const submitHandler = async () => {
+    setLoading(true);
+    if (!email || !password) {
+      toast.show({
+        description: "Please fill all the fields.",
+      });
+      setLoading(false);
+      return;
+    }
+
+    try {
+      const config = {
+        headers: {
+          "Content-type": "application/json",
+        },
+      };
+      const { data } = await axios.post(
+        "/api/user/login",
+        { email, password },
+        config
+      );
+      toast({
+        description: "Login successful.",
+      });
+      // localStorage.setItem("userInfo", JSON.stringify(data));
+      setLoading(false);
+      //history.push("/chats");
+    } catch (error) {
+      toast({
+        description: "Error occurred.",
+        //description: error.response.data.message,
+      });
+      setLoading(false);
+    }
+  };
 
   return (
     <>
       <Heading margin-top="0">Log in</Heading>
       <Container>
-        <FormControl>
+        <FormControl id="email" isRequired>
           <FormControl.Label>Email</FormControl.Label>
-          <Input placeholder="Email" w="100%" py="3" />
+          <Input
+            placeholder="Email"
+            onChangeText={(value) => setEmail(value)}
+            w="100%"
+            py="3"
+          />
         </FormControl>
 
-        <FormControl mt="3">
+        <FormControl id="password" isRequired mt="3">
           <FormControl.Label>Password</FormControl.Label>
           <Input
             type={show ? "text" : "password"}
             w="100%"
             py="3"
+            onChangeText={(value) => setPassword(value)}
             InputRightElement={
               <Button
+                onPress={handleClick}
                 size="xs"
                 rounded="none"
                 w="1/6"
                 h="full"
-                onPress={handleClick}
               >
                 {show ? "Hide" : "Show"}
               </Button>
@@ -44,7 +95,7 @@ export const LoginScreen = () => {
           />
         </FormControl>
         <Box>
-          <Button>Log in</Button>
+          <Button onPress={submitHandler}>Log in</Button>
         </Box>
       </Container>
     </>
