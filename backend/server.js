f4bcef6f15e9179c830adc6e81ddcfc401889088
PiotrLehmann <piterrubik@gmail.com>
2023-04-14 03:24:38 +0200

    Error handlers done

--- backend/server.js
+++ backend/server.js
@@ -3,6 +3,7 @@ const dotenv = require("dotenv");
 const { chats } = require("./data/data");
 const connectDB = require("./config/db");
 const userRoutes = require("./routes/userRoutes");
+const { errorHandler, notFound } = require('./middleware/errorMiddleware');
 
 dotenv.config();
 connectDB();
@@ -16,6 +17,9 @@ app.get("/", (req, res) => {
 });
 
 app.use("/api/user", userRoutes);
+
+app.use(notFound);
+app.use(errorHandler);
 
 const PORT = process.env.PORT || 5000;
 
