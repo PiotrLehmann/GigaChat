4b293142865710e7fa448238b8da4276ea937c62
PiotrLehmann <piterrubik@gmail.com>
2023-04-14 03:51:32 +0200

    Picture Upload to Cloudinary done

--- frontend/src/components/Authentication/SignUp.js
+++ frontend/src/components/Authentication/SignUp.js
@@ -9,6 +9,7 @@ import {
   VStack,
 } from "@chakra-ui/react";
 import { useState } from "react";
+import { useToast } from '@chakra-ui/react'
 
 const SignUp = () => {
   const [name, setName] = useState();
@@ -17,12 +18,55 @@ const SignUp = () => {
   const [password, setPassword] = useState();
   const [pic, setPic] = useState();
   const [show, setShow] = useState(false);
+  const [loading, setLoading] = useState();
+  const toast = useToast();
+
 
   const handleClick = () => {
     setShow(!show);
   };
 
-  const postDetails = (pics) => {};
+  const postDetails = (pics) => {
+    setLoading(true);
+    if (pics === undefined) {
+      toast({
+        title: 'Please select an image.',
+        status: 'warning',
+        duration: 5000,
+        isClosable: true,
+        position: "bottom",
+      });
+      return;
+    }
+
+    if(pics.type === "image/jpeg" || pics.type === "image/png") {
+      const data = new FormData();
+      data.append("file", pics);
+      data.append("upload_preset", "Gigachat");
+      data.append("cloud_name", "ddyw43b02");
+      fetch("https://api.cloudinary.com/v1_1/ddyw43b02/image/upload", {
+        method: 'post', body: data,
+      }).then((res) => res.json())
+      .then(data => {
+        setPic(data.url.toString());
+        setLoading(false);
+      })
+      .catch((err) => {
+        console.log(err);
+        setLoading(false);
+      });
+    } else {
+      toast({
+        title: 'Please select an image.',
+        status: 'warning',
+        duration: 5000,
+        isClosable: true,
+        position: "bottom",
+      });
+      setLoading(false)
+      return;
+    }
+  };
 
   const submitHandler = () => {};
 
@@ -81,7 +125,7 @@ const SignUp = () => {
           onChange={(e) => postDetails(e.target.files[0])}
         />
       </FormControl>
-      <Button width="100%" onClick={submitHandler}>
+      <Button width="100%" onClick={submitHandler} isLoading={loading}>
         Sign Up
       </Button>
     </VStack>
