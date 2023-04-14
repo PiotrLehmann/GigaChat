30ac3fbfaa6fb1717b748d7295ed455a9ef49940
Bart <barteqczech@gmail.com>
2023-04-15 00:09:04 +0200

    Search User API: done

--- backend/routes/userRoutes.js
+++ backend/routes/userRoutes.js
@@ -1,9 +1,10 @@
 const express = require("express");
 const { registerUser } = require('../controllers/userControllers');
 const { authUser } = require('../controllers/userControllers');
+const { allUsers } = require('../controllers/userControllers');
 const router = express.Router();
 
-router.route("/").post(registerUser);
+router.route("/").post(registerUser).get(allUsers);
 router.post("/login", authUser);
 
 module.exports = router;
