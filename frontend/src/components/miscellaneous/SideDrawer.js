8c8bf13614a3577a705ff64e0aa49f62f8800748
PiotrLehmann <piterrubik@gmail.com>
2023-04-15 21:38:48 +0200

    Header done

--- frontend/src/components/miscellaneous/SideDrawer.js
+++ frontend/src/components/miscellaneous/SideDrawer.js
@@ -1,7 +1,75 @@
-import React from "react";
+import React, { useState } from "react";
+import {
+  Tooltip,
+  Menu,
+  MenuItem,
+  MenuButton,
+  MenuList,
+  Avatar,
+  MenuDivider,
+} from "@chakra-ui/react";
+import { BellIcon, ChevronDownIcon } from "@chakra-ui/icons";
+import { Box, Text } from "@chakra-ui/layout";
+import { Button } from "@chakra-ui/button";
+import { ChatState } from "../../Context/ChatProvider";
+import ProfileModel from "./ProfileModel";
 
 const SideDrawer = () => {
-  return <div>SideDrawer</div>;
+  const [search, setSearch] = useState("");
+  const [searchResult, setSearchResult] = useState([]);
+  const [loading, setLoading] = useState(false);
+  const [loadingChat, setLoadingChat] = useState();
+
+  const { user } = ChatState();
+
+  return (
+    <>
+      <Box
+        display="flex"
+        justifyContent="space-between"
+        alignItems="center"
+        background="white"
+        w="100%"
+        p="5px 10px 5px 10px"
+        borderWidth="5px"
+      >
+        <Tooltip label="Search Users to chat" hasArrow placement="bottom-end">
+          <Button variant="ghost" background="white" w="100px" h="30px">
+            <Text display={{ base: "none", md: "flex" }}>Search User</Text>
+          </Button>
+        </Tooltip>
+
+        <Text fontSize="2xl" fontFamily="Arial">
+          GIGACHAT
+        </Text>
+        <div>
+          <Menu>
+            <MenuButton p={1}>
+              <BellIcon fontSize="2xl" m={1} />
+            </MenuButton>
+            {/* <MenuList></MenuList> */}
+          </Menu>
+          <Menu>
+            <MenuButton as={Button} rightIcon={<ChevronDownIcon />}>
+              <Avatar
+                size="sm"
+                cursor="pointer"
+                name={user.name}
+                src={user.pic}
+              />
+            </MenuButton>
+            <MenuList>
+              <ProfileModel user={user}>
+                <MenuItem>My Profile</MenuItem>
+              </ProfileModel>
+              <MenuDivider />
+              <MenuItem>Log out</MenuItem>
+            </MenuList>
+          </Menu>
+        </div>
+      </Box>
+    </>
+  );
 };
 
 export default SideDrawer;
