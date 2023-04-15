0351d0e94477858035df825e0bea37e4349c4d81
PiotrLehmann <piterrubik@gmail.com>
2023-04-15 19:17:45 +0200

    ChatsPage template working

--- frontend/src/pages/Chatpage.js
+++ frontend/src/pages/Chatpage.js
@@ -1,20 +1,27 @@
 import React, { useEffect, useState } from "react";
 import axios from "axios";
 import { ChatState } from "../Context/ChatProvider";
-import { Box } from "@chakra-ui/react";
 import SideDrawer from "../components/miscellaneous/SideDrawer";
+import { Box } from "@chakra-ui/layout";
+import MyChats from "../components/MyChats";
+import ChatBox from "../components/ChatBox";
 
 const Chatpage = () => {
   const { user } = ChatState();
 
   return (
     <div style={{ width: "100%" }}>
-      {/* {user && <SideDrawer />} */}
-      {/* <Box> */}
-      {/* {user && <MyChats />} */}
-      {/* {user && <ChatBox />} */}
-
-      {/* </Box> */}
+      {user && <SideDrawer />}
+      <Box
+        display="flex"
+        justifyContent="space-between"
+        w="100%"
+        h="91.5vh"
+        p="10px"
+      >
+        {user && <MyChats />}
+        {user && <ChatBox />}
+      </Box>
     </div>
   );
 };
