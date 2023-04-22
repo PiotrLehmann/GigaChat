b0ddd58b5eadd8bf014dea800ae77599a3b9b4d2
Bart <barteqczech@gmail.com>
2023-04-22 19:24:27 +0200

    React.js: Basic SingleChat UI

---
+++ frontend/src/components/SingleChat.js
@@ -0,0 +1,54 @@
+import React from "react";
+import { ChatState } from "../Context/ChatProvider";
+import { Box, IconButton, Text } from "@chakra-ui/react";
+import { ArrowBackIcon } from "@chakra-ui/icons";
+import { getSender, getSenderFull } from "../config/ChatLogics";
+import ProfileModel from "./miscellaneous/ProfileModal";
+
+const SingleChat = ({ fetchAgain, setFetchAgain }) => {
+  const { user, selectedChat, setSelectedChat } = ChatState();
+  return (
+    <>
+      {selectedChat ? (
+        <>
+          <Text
+            fontSize={{ base: "28px", md: "30px" }}
+            pb={3}
+            px={2}
+            w="100%"
+            d="flex"
+            justifyContent={{ base: "space-between" }}
+            alignItems="center"
+          >
+            <IconButton
+              d={{ base: "flex", md: "none" }}
+              icon={<ArrowBackIcon />}
+              onClick={() => setSelectedChat("")}
+            />
+            {!selectedChat.isGroupChat ? (
+              <>{getSender(user, selectedChat.users)}
+              <ProfileModel user={getSenderFull(user, selectedChat.users)}/>
+              </>
+            ) : (
+              <>
+                {selectedChat.chatName.toUpperCase()}
+                {/* <UpdateGroupChatModal
+                  fetchAgain={fetchAgain}
+                  setFetchAgain={setFetchAgain}
+                /> */}
+              </>
+            )}
+          </Text>
+        </>
+      ) : (
+        <Box d="flex" alignItems="center" justifyContent="center" h="100%">
+          <Text fontSize="3xl" pb={3}>
+            Click on a user to start chatting
+          </Text>
+        </Box>
+      )}
+    </>
+  );
+};
+
+export default SingleChat;
