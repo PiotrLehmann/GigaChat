43da1cbaeeadd0d1505d70b067d34f153cb83e38
Bart <barteqczech@gmail.com>
2023-05-11 13:18:17 +0200

    removed old apk file

--- native_frontend/app.config.js
+++ native_frontend/app.config.js
@@ -1,10 +1,15 @@
 import "dotenv/config";
 
 export default {
-  expo: {
+  "expo": {
+    "extra": {
+      "eas": {
+        "projectId": "55ba4b56-c98c-4658-8042-8a6c58d27c93"
+      }
+    },
     name: "Gigachat",
     slug: "Gigachat",
-    version: "1.0.0",
+    version: "1.0.1",
     orientation: "portrait",
     icon: "./assets/icon.png",
     userInterfaceStyle: "light",
@@ -17,11 +22,9 @@ export default {
     ios: {
       supportsTablet: true,
     },
-    android: {
-      adaptiveIcon: {
-        foregroundImage: "./assets/adaptive-icon.png",
-        backgroundColor: "#ffffff",
-      },
+    "android": {
+      "package": "com.yourcompany.yourappname",
+      "versionCode": 1
     },
     web: {
       favicon: "./assets/favicon.png",
